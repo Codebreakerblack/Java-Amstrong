452a8aec1aa7de7bce553f04f26bf404279de869
Codebreakerblack <blancohmariel@gmail.com>
2024-11-14 16:05:27 +0100

    classes NumArmstrong with a method that calculates the Amstrong number with a loop, a method that checks this number to tell the console if it is true or false and a main method class App for printing in the console

---
+++ src/main/java/dev/mariel/armstrong/App.java
@@ -0,0 +1,18 @@
+package dev.mariel.armstrong;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+3
+        System.out.print("Ingrese un número: ");
+        int num = scanner.nextInt();
+
+        System.out.println(
+                (NumArmstrong.checkArmstrong(num)) ? num + " es un Armstrong" : num + " no es un Armstrong");
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/dev/mariel/armstrong/NumArmstrong.java
@@ -0,0 +1,23 @@
+package dev.mariel.armstrong;
+
+public class NumArmstrong {
+
+    private static boolean isArmstrong(int num) {
+        int numOrigin = num;
+        int digits = (int)(Math.log10(num) + 1);
+        int sum = 0;
+
+        while (num >0) {
+            int lastDigit = num % 10;
+            sum += Math.pow(lastDigit, digits);
+            num /= 10; 
+        }
+
+        return numOrigin == sum;
+    }
+
+    public static boolean checkArmstrong(int num) {
+        return isArmstrong(num);
+    }
+
+}
